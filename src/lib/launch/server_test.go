00d27ff114310bd78cb1da25ca7a1f74648e3ef6
dbschrooten <dbschrooten@gmail.com>
2020-10-17 10:28:16 +0200

    Add travis-ci

--- src/lib/launch/server_test.go
+++ src/lib/launch/server_test.go
@@ -11,4 +11,10 @@ func TestServer(t *testing.T) {
 	if err := Server(); err != nil {
 		t.Error(err)
 	}
+
+	os.Setenv("AM_CONFIG_PATH", "src/config.toml")
+
+	if err := Server(); err == nil {
+		t.Errorf("Should return config not found error")
+	}
 }
