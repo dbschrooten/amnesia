4dfa894640cf1d6e45dd3aa026ba274fccd81dd8
dbschrooten <dbschrooten@gmail.com>
2020-10-23 20:59:12 +0200

    Use viper for config

--- src/config/setup.go
+++ src/config/setup.go
@@ -1,119 +1,56 @@
 package config
 
 import (
-	"amnesia/src/helpers"
 	"amnesia/src/service"
 	"fmt"
-	"log"
-	"time"
 
-	"github.com/BurntSushi/toml"
-	"github.com/mitchellh/mapstructure"
+	"github.com/spf13/viper"
 )
-
-type ConfigDefaults struct {
-	Interval time.Duration
-	Timeout  time.Duration
-}
 
 var (
-	Defaults ConfigDefaults
-	Services []service.Service
+	Services = make(map[string]service.Service)
 )
 
-func mapDefaults(config map[string]interface{}) error {
-	var (
-		defaultInterval time.Duration
-		defaultTimeout  time.Duration
-		err             error
-	)
+func init() {
+	viper.SetConfigName("config")
+	viper.AddConfigPath("./")
 
-	if _, ok := config["default"]; !ok {
-		defaultInterval, err = time.ParseDuration(DefaultInterval)
-		defaultTimeout, err = time.ParseDuration(DefaultTimeout)
-	} else {
-		if _, ok := config["default"].(map[string]interface{})["interval"]; !ok {
-			defaultInterval, err = time.ParseDuration(DefaultInterval)
-		} else {
-			defaultInterval, err = time.ParseDuration(
-				config["default"].(map[string]interface{})["interval"].(string),
-			)
-		}
+	viper.SetDefault("default.interval", "5m")
+	viper.SetDefault("default.timeout", "30s")
+	viper.SetDefault("default.language", "en")
+	viper.SetDefault("default.retention", "7d")
 
-		if _, ok := config["default"].(map[string]interface{})["timeout"]; !ok {
-			defaultTimeout, err = time.ParseDuration(DefaultTimeout)
-		} else {
-			defaultTimeout, err = time.ParseDuration(
-				config["default"].(map[string]interface{})["timeout"].(string),
-			)
-		}
-	}
+	viper.SetDefault("server.address", "localhost:2600")
+	viper.SetDefault("server.write_timeout", "15s")
+	viper.SetDefault("server.read_timeout", "15s")
 
-	if err != nil {
+	viper.SetDefault("system.plugin_folder", "./plugins")
+}
+
+func mapServices() error {
+	var svc []service.Service
+
+	if err := viper.UnmarshalKey("service", &svc); err != nil {
 		return err
 	}
 
-	Defaults.Interval = defaultInterval
-	Defaults.Timeout = defaultTimeout
-
-	return nil
-}
-
-func mapServices(config map[string]interface{}) error {
-	// Parse Services
-	for _, Service := range config["service"].([]map[string]interface{}) {
-		if !helpers.StringInSlice(Service["type"].(string), service.ServiceTypes()) {
-			return fmt.Errorf("Config contains non-existing service type %s", Service["type"].(string))
-		}
-
-		var serviceConfig service.Service
-		mapstructure.Decode(Service, &serviceConfig)
-
-		// do more validation in here
-		if Service["interval"] != nil {
-			intVal, err := time.ParseDuration(Service["interval"].(string))
-
-			if err != nil {
-				return err
-			}
-
-			serviceConfig.Interval = intVal
-		} else {
-			serviceConfig.Interval = Defaults.Interval
-		}
-
-		if Service["timeout"] != nil {
-			timeOut, err := time.ParseDuration(Service["timeout"].(string))
-
-			if err != nil {
-				return err
-			}
-
-			serviceConfig.Timeout = timeOut
-		} else {
-			serviceConfig.Timeout = Defaults.Timeout
-		}
-
-		Services = append(Services, serviceConfig)
+	for _, s := range svc {
+		Services[s.ID] = s
 	}
 
 	return nil
 }
 
 func Setup() error {
-	var config map[string]interface{}
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			return fmt.Errorf("Config file not found")
+		}
 
-	if _, err := toml.DecodeFile(ConfigPath, &config); err != nil {
 		return err
 	}
 
-	log.Print("Load config.toml")
-
-	if err := mapDefaults(config); err != nil {
-		return err
-	}
-
-	if err := mapServices(config); err != nil {
+	if err := mapServices(); err != nil {
 		return err
 	}
 
